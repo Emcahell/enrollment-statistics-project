21f7301fd4357e94488839d4c9a841d98d85e237
emcahell <emmanuelltowers@gmail.com>
2025-04-03 20:57:06 -0400

    mejore la ui

--- App/helpers/core.js
+++ App/helpers/core.js
@@ -213,26 +213,4 @@ buttonSave.addEventListener('click', (e) => {
   
 });
 
-// mostrar historial
-// document.addEventListener('DOMContentLoaded', mostrarHistorial);
-// const mostrarHistorial = () => {
-//   console.log("Ejecutando mostrarHistorial");
-//     contenedorHistorial.innerHTML = '';
-//     let historial = JSON.parse(localStorage.getItem('historial')) || [];
-//     historial.forEach((item) => {
-//         const card = document.createElement('div');
-//         card.className = 'card';
-//         card.innerHTML = `<h3>${item.mes}</h3>
-//                           <table>
-//                             <tr><td>Prov</td><td>${item.prov}</td></tr>
-//                             <tr><td>Proh</td><td>${item.proh}</td></tr>
-//                             <tr><td>Porv</td><td>${item.porv}%</td></tr>
-//                             <tr><td>Porh</td><td>${item.porh}%</td></tr>
-//                           </table>`;
-//         contenedorHistorial.appendChild(card);
-//     });
-//   }
-// document.addEventListener('DOMContentLoaded', mostrarHistorial);
-
-
 }
